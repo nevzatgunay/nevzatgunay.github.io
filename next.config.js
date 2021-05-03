b1ae07e46c4cbcbac6cb87616b499dacaf92f76d
Nevzat Günay <gunaynevzat@yandex.com>
2021-05-04 00:44:04 +0300

    Remove basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([optimizedImages], {
-  basePath: '/nevzatgunay.github.io',
-  assetPrefix: '/nevzatgunay.github.io',
-});
+module.exports = withPlugins([optimizedImages]);
