ca245176e9769f5f8c196e2d1c81d5a847097e1b
Nevzat Günay <gunaynevzat@yandex.com>
2021-05-03 18:43:26 +0300

    Add homepage to package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([
-  optimizedImages,
-]);
+module.exports = withPlugins([optimizedImages]);
