5ff1401d4f959e47291ba055e9d667cc46e0a2a5
Nevzat Günay <gunaynevzat@yandex.com>
2021-05-03 18:19:26 +0300

    Add Link as prop

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Navigation = (): JSX.Element => {
   return (
     <nav>
-      <Link href="/">
+      <Link href="/" as={`/`}>
         <a className="text-gray-900 dark:text-white pr-6 py-4">Home</a>
       </Link>
-      <Link href="/projects">
+      <Link href="/projects" as={`/projects`}>
         <a className="text-gray-900 dark:text-white px-6 py-4">Projects</a>
       </Link>
     </nav>
